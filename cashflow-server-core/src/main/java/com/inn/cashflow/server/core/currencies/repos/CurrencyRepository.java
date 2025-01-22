5215c66c07247bee4c3fc72c3e3125c05fde8f60
Tushar Nagdive <codewithtushar840@gmail.com>
2025-01-22 18:59:32 +0530

    change

--- cashflow-server-core/src/main/java/com/inn/cashflow/server/core/currencies/repos/CurrencyRepository.java
+++ cashflow-server-core/src/main/java/com/inn/cashflow/server/core/currencies/repos/CurrencyRepository.java
@@ -1,7 +1,5 @@
 package com.inn.cashflow.server.core.currencies.repos;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
